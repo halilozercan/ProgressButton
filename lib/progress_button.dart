2bfab6b5df2f3898ebe439af2f4eb4ee091a6877
Matt Griffiths <matt.griffiths@chester.ac.uk>
2019-04-16 11:50:50 +0100

    Refactor isAnimationRunning()

--- lib/progress_button.dart
+++ lib/progress_button.dart
@@ -104,7 +104,7 @@ class _ProgressButtonState extends State<ProgressButton>
 
   /// A utility function to check whether an animation is running
   bool isAnimationRunning(AnimationController controller) {
-    return controller.isCompleted && controller.isDismissed;
+    return !controller.isCompleted && !controller.isDismissed;
   }
 
   @override
@@ -148,7 +148,7 @@ class _ProgressButtonState extends State<ProgressButton>
 
     Widget buttonContent;
 
-    if (widget.buttonState != ButtonState.inProgress &&
+    if (widget.buttonState != ButtonState.inProgress ||
         !isAnimationRunning(_progressAnimationController)) {
       buttonContent = child;
 
